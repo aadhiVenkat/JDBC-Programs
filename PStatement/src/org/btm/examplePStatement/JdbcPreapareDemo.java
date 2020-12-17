02ac9da6316ba37e0c11e145c2b235fcc79380f6
aadhimca <adi>
2020-12-17 21:58:05 +0530

    Enter Multiple Records Using Prepared Statement Adding Final Block

--- PStatement/src/org/btm/examplePStatement/JdbcPreapareDemo.java
+++ PStatement/src/org/btm/examplePStatement/JdbcPreapareDemo.java
@@ -15,18 +15,34 @@ public class JdbcPreapareDemo {
 			System.out.println("Connection Established");
 			ps=con.prepareStatement(sql);//Compile once
 			System.out.println("Platform Created");
-			ps.setInt(1, 2);
-			ps.setString(2, "Adi");
-			ps.setDouble(3, 90.0);
+			ps.setInt(1, 4);
+			ps.setString(2, "Mahesh");
+			ps.setDouble(3, 80.05);
 			ps.execute();
 			
-			ps.setInt(1, 3);
-			ps.setString(2, "Venkat");
-			ps.setDouble(3, 99.0);
+			ps.setInt(1, 5);
+			ps.setString(2, "Babu");
+			ps.setDouble(3, 85.05);
 			ps.execute();
 			
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
+		}
+		finally
+		{
+			if(ps!=null)
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			if(con!=null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			System.out.println("!!!!All Costly Resoursec are Closed!!!!");
 		}
 		
 	}
